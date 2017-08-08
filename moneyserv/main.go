7bb7a9c15fe97fa955b1a7724e6281048e845ed7
Andrew Kallmeyer <fsmv@sapium.net>
2017-08-07 21:13:52 -0700

    Added basic CSV serving functionality to play around with it This was actually written last night

--- moneyserv/main.go
+++ moneyserv/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+    "bytes"
     "os"
     "os/signal"
     "fmt"
@@ -9,8 +10,37 @@ import (
     "time"
     "net/http"
     "net/rpc"
+    "strings"
     "strconv"
+    "sync"
 )
+
+var (
+    csvbuf  bytes.Buffer
+    counter int
+    mut     sync.Mutex
+)
+
+func addNRows(n int) error {
+    mut.Lock()
+    defer mut.Unlock()
+    for n += counter; counter < n; counter++ {
+        row := fmt.Sprintf("%v,%v\n", counter, counter*counter)
+        _, err := csvbuf.WriteString(row)
+        if err != nil {
+            return err
+        }
+    }
+    return nil
+}
+
+func getCSV() []byte {
+    mut.Lock()
+    defer mut.Unlock()
+    return csvbuf.Bytes()
+}
+
+const register = "/"
 
 func main() {
     sigs := make(chan os.Signal, 2)
@@ -23,27 +53,43 @@ func main() {
     defer client.Close()
 
     var lease proxyserv.Lease
-    err = client.Call("feproxy.Register", "/test/", &lease)
+    err = client.Call("feproxy.Register", register, &lease)
     if err != nil {
         log.Fatal("Failed to obtain lease from feproxy:", err)
     }
     log.Printf("Obtained lease, port: %v, ttl: %v", lease.Port, lease.TTL)
 
-    defer client.Call("feproxy.Deregister", "/test/", &lease)
+    defer client.Call("feproxy.Deregister", register, &lease)
     go func() {
         <-sigs
-        client.Call("feproxy.Deregister", "/test/", &lease)
+        client.Call("feproxy.Deregister", register, &lease)
         log.Print("Shutting down...")
         // TODO(1.8): use http.Server and call close here
         os.Exit(0)
     }()
 
-    http.HandleFunc("/test/", func(w http.ResponseWriter, r *http.Request) {
-        fmt.Fprint(w, "<body><h3>Hello, World!</h3>")
-        fmt.Fprint(w, "Your url: ", r.URL.Path)
-        fmt.Fprint(w, "</body>")
+    http.HandleFunc("/data.csv", func(w http.ResponseWriter, r *http.Request) {
+        w.Header().Set("Content-Type", "text/csv")
+        w.Write(getCSV())
     })
-    http.HandleFunc("/test/quit", func(w http.ResponseWriter, r *http.Request) {
+    http.HandleFunc("/add/", func(w http.ResponseWriter, r *http.Request) {
+        w.Header().Set("Content-Type", "text/html")
+        fmt.Fprint(w, "<html><body><h2>\n")
+        defer fmt.Fprint(w, "</h2></body></html>\n")
+        path := r.URL.Path
+        lastSlash := strings.LastIndex(path, "/")
+        if lastSlash != len(path) - 1{
+            num, err := strconv.Atoi(path[lastSlash+1:])
+            if err != nil {
+                fmt.Fprint(w, "Error, should use /add/{number}")
+                return
+            }
+            addNRows(num)
+        } else {
+            addNRows(1)
+        }
+    })
+    http.HandleFunc("/quit", func(w http.ResponseWriter, r *http.Request) {
         fmt.Fprint(w, "<body><h3>Shutting down!</h3></body>")
         time.AfterFunc(time.Second, func () {
             client.Call("feproxy.Quit", struct{}{}, struct{}{})
