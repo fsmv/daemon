ac13dee45a067ecb0545f6435c7ba01d527906d2
Andrew Kallmeyer <ask@ask.systems>
2022-05-01 15:16:20 -0700

    Make the servers gracefully shut down when they are killed with killall

--- tools/signals.go
+++ tools/signals.go
@@ -4,17 +4,20 @@ import (
   "log"
   "os"
   "os/signal"
+  "syscall"
 )
 
 func CloseOnSignals(quit chan struct{}) {
   sigs := make(chan os.Signal, 2)
-  signal.Notify(sigs, os.Interrupt, os.Kill)
+  signal.Notify(sigs, os.Interrupt, os.Kill, syscall.SIGTERM)
   go func() {
     switch <-sigs {
     case os.Interrupt:
       log.Print("Recieved keyboard interrupt")
     case os.Kill:
       log.Print("Recieved kill signal")
+    case syscall.SIGTERM:
+      log.Print("Recieved term signal")
     }
     close(quit)
   }()
