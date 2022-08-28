0c83f5186adf243f8f9bad3cb381127ededb21a4
Andrew Kallmeyer <ask@ask.systems>
2022-08-28 00:20:11 -0700

    Support remote syslogging, probably the easiest method for chroot

--- tools/flags/syslog_flag.go
+++ tools/flags/syslog_flag.go
@@ -7,6 +7,8 @@ import (
   "log"
   "log/syslog"
   "path/filepath"
+  "strings"
+  "errors"
 
   "ask.systems/daemon/tools"
 )
@@ -17,26 +19,52 @@ var (
   //
   // Using this writer directly will not also log to stdout
   Syslog *syslog.Writer
+
 )
 
 func init () {
-  flag.Var(tools.BoolFuncFlag(handleSyslogFlag), "use_syslog",
+  flag.Var(tools.BoolFuncFlag(handleSyslogFlag), "syslog",
     "If set, log to the syslog service in addition to stdout when using the go\n"+
     "log package. Logs under user.info (facility.severity). See also: man syslog.\n"+
-    "To use this in a chroot configure syslogd with the -l flag to create the <chroot>/dev/log file.")
+
+    "\nTo use this in a chroot you can setup networking and use the -syslog_remote flag\n"+
+    "or configure syslogd with the -l flag to create the <chroot>/dev/log file.")
+  flag.Func("syslog_remote",
+    "Set to your url or ip for UDP remote logging. Optionally prefix with\n"+
+    "tcp:// or any other https://pkg.go.dev/net#Dial supported protocols to connect\n"+
+    "to syslog servers that support other protocols.\n"+
+    "\nIf you're using -syslog_remote, do not set -syslog.",
+    handleSyslogFlag)
 }
 
 func handleSyslogFlag(value string) error {
+  if Syslog != nil {
+    return errors.New("Syslog was already loaded, use only one of -syslog or -syslog_remote.")
+  }
   log.Print("Loading syslog...")
   var err error
-  // TODO: it would be nice to read the flag value as an address to optionally
-  // do remote logging
-  Syslog, err = syslog.New(syslog.LOG_INFO | syslog.LOG_USER, filepath.Base(os.Args[0]))
+  var network, addr string
+  if value != "true" && value != "" {
+    network_idx := strings.Index(value, "://")
+    if network_idx != -1 {
+      network = value[:network_idx]
+      addr = value[network_idx+3:]
+    } else {
+      network = "udp"
+      addr = value
+      if strings.Index(addr, ":") == -1 {
+        addr += ":514" // default syslog port
+      }
+    }
+  }
+  // if network and addr are empty it tries using the system files
+  Syslog, err = syslog.Dial(network, addr,
+    syslog.LOG_INFO | syslog.LOG_USER, filepath.Base(os.Args[0]))
   if err != nil {
     return err
   }
-  log.SetFlags(0) // Just use the syslog built in timestamp
+  log.SetFlags(0) // Don't add timestamps because syslog adds them
   log.SetOutput(io.MultiWriter(Syslog,
-    tools.NewTimestampWriter(os.Stdout)))
+    tools.NewTimestampWriter(os.Stdout))) // But still print timestamps
   return nil
 }
