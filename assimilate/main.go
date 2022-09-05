c6d8921b8e9e404936c5ead741f06a3816601b81
Andrew Kallmeyer <ask@ask.systems>
2022-09-04 19:19:38 -0700

    Change assimilate to use text proto flags instead of json flags matches the new spawn config format

--- assimilate/main.go
+++ assimilate/main.go
@@ -1,28 +1,50 @@
 package main
 
 import (
-  "flag"
+  "os"
   "log"
+  "fmt"
+  "flag"
   "sync"
+  "strings"
   "context"
 
+  _ "embed"
   _ "ask.systems/daemon/tools/flags"
 
   "ask.systems/daemon/portal"
   "ask.systems/daemon/tools"
+
+  "google.golang.org/protobuf/encoding/prototext"
 )
 
 var (
   portalAddr = flag.String("portal_addr", "127.0.0.1:2048",
     "Address and port for the portal server")
-  registrations portal.RegisterRequests
 )
 
+// go:embed ../portal/service.proto
+var schemaText string
+
 func init() {
-  flag.Var(&registrations, "+",
-    "A JSON flag for mapping third party web servers to a url path.\n." +
-    "Uses the portal/service.proto RegisterRequests json format.\n" +
-    "For example:" + `'{"requests": [{"fixed_port": 1337, "pattern" : "/your-server", "strip_pattern": true}, {...}, ...]}'`)
+  flag.Usage = func () {
+    fmt.Fprintf(flag.CommandLine.Output(),
+      "Usage: %s [flags] \"[textproto portal.RegisterRequest]\"...\n", os.Args[0])
+    // TODO
+    fmt.Fprintf(flag.CommandLine.Output(),
+    "Example:\n" + 
+    "  %v -portal_addr localhost:9999 \"pattern: '/test/' fixed_port: 8080 strip_pattern: true\" \\\n" +
+    "    \"pattern: ':tcp:8181' fixed_port: 1337\"\n\n", os.Args[0])
+    flag.PrintDefaults()
+  }
+  flag.Var(tools.BoolFuncFlag(func(string) error {
+      start := strings.Index(portal.ServiceProto, "message RegisterRequest")
+      end := strings.Index(portal.ServiceProto[start:], "}")
+      fmt.Println(portal.ServiceProto[start:start+end+1])
+      os.Exit(2)
+      return nil
+    }), "register_request_schema",
+    "Print the schema for portal.RegisterRequest in proto form and exit.")
   flag.Parse()
 }
 
@@ -34,7 +56,14 @@ func main() {
     log.Fatal(err)
   }
   errCount := 0
-  for i, registration := range registrations.Requests {
+  for i, requestText := range flag.Args() {
+    registration := &portal.RegisterRequest{}
+    err := prototext.Unmarshal([]byte(requestText), registration)
+    if err != nil {
+      log.Printf("Failed to unmarshal RegisterRequest %#v: %v", requestText, registration)
+      errCount++
+      continue
+    }
     lease, err := fe.RPC.Register(context.Background(), registration)
     if err != nil {
       log.Printf("Failed to register #%v %+v: %v", i, registration, err)
@@ -42,14 +71,14 @@ func main() {
       continue
     }
     log.Printf("Obtained lease for %#v, port: %v, timeout: %v",
-    lease.Pattern, lease.Port, lease.Timeout.AsTime())
+      lease.Pattern, lease.Port, lease.Timeout.AsTime())
     wg.Add(1)
     go func () {
       fe.KeepLeaseRenewed(quit, lease)
       wg.Done()
     }()
   }
-  if errCount == len(registrations.Requests) {
+  if errCount == len(flag.Args()) {
     close(quit)
     wg.Wait()
     log.Fatal("None of the registrations were successful.")
