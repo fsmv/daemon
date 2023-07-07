b07398fecd13d7a8d396d47778890e3c93ff0daf
Andrew Kallmeyer <ask@ask.systems>
2023-07-06 22:26:02 -0700

    For macos support attempt to read the LC_LOAD_DYLINKER mach-o section
    This is an extra file needed in the chroot just like on Linux.

--- spawn/embedspawn/children_darwin.go
+++ spawn/embedspawn/children_darwin.go
@@ -1,7 +1,9 @@
 package embedspawn
 
 import (
+	"bytes"
 	"debug/macho"
+	"encoding/binary"
 	"errors"
 	"io/fs"
 	"os"
@@ -10,6 +12,21 @@ import (
 
 func maybeSetDeathSig(attr *os.ProcAttr) {
 	// macos doesn't support Pdeathsig
+}
+
+// The macho library doesn't provide this!
+//
+// I based this struct on the others like macho.DylibCmd and the output of
+// objtool -l which printed:
+//
+//	Load command 9
+//	     cmd LC_LOAD_DYLINKER
+//	 cmdsize 32
+//	    name /usr/lib/dyld (offset 12)
+type dylinkerCmd struct {
+	Cmd  macho.LoadCmd
+	Len  uint32
+	Name uint32
 }
 
 func requiredLibsImpl(paths []string, filename string, libs map[string]struct{}, interp map[string]struct{}) error {
@@ -22,7 +39,35 @@ func requiredLibsImpl(paths []string, filename string, libs map[string]struct{},
 		return err
 	}
 
-	// TODO: Read the path to the ld shared library which is also needed?
+	// Read the path to the ld shared library which is also needed?
+	for _, load := range bin.Loads {
+		loadBytes, ok := load.(macho.LoadBytes)
+		if !ok {
+			continue
+		}
+		if len(loadBytes) < 4 {
+			continue
+		}
+		// https://cs.opensource.google/go/go/+/master:src/debug/macho/file.go;l=277;drc=e8fbad5de87f34d2e7632f94cac418c7436174ce
+		cmd := macho.LoadCmd(bin.ByteOrder.Uint32(loadBytes[0:4]))
+		if cmd != macho.LoadCmdDylinker {
+			continue
+		}
+		// Based the following code on:
+		// https://cs.opensource.google/go/go/+/master:src/debug/macho/file.go;l=306,313;drc=e8fbad5de87f34d2e7632f94cac418c7436174ce
+		var hdr dylinkerCmd
+		if err := binary.Read(bytes.NewReader(loadBytes), bin.ByteOrder, &hdr); err != nil {
+			return err
+		}
+		cstrName := loadBytes[hdr.Name:]
+		i := bytes.IndexByte(cstrName, 0)
+		if i == -1 {
+			i = len(cstrName)
+		}
+		dylinker := string(cstrName[0:i])
+		interp[dylinker] = struct{}{}
+		break
+	}
 
 	// Read the libraries used by the binary (loaded by interp)
 	imports, err := bin.ImportedLibraries()
