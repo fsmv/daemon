fsmv/daemon

commit cb6357666d30f2b31639a1d9274fbd0e9708f9d0

Detect TLS support on backends even if they use a self signed cert
I realized when I added the hostname request parameter that I was making
it easy to send unencrypted data over the internet. Also the same thing
already happened if you use assimilate over the internet instead of a
local network.

So to do something about that, now if you configure your backend server
to use a self-signed cert, portal will connect with it and encrypt the
connection.

I still need to add a feature to assimilate where it will write a
portal-signed certificate to a directory of your choosing but I haven't
decided on the arguments API to do that yet. For now assimilate should
be used on the same machine as portal or a trusted local network.

portal/embedportal/http_proxyserv.go
@@ -10,7 +10,6 @@ import (
 	"net/http/httputil"
 	"net/url"
 	"os"
-	"strconv"
 	"strings"
 	"sync"
 
@@ -82,8 +81,7 @@ func (p *httpProxy) Register(
 		return nil, err
 	}
 
-	useTLS := len(request.CertificateRequest) != 0
-	err = p.saveForwarder(clientAddr, lease, request.StripPattern, request.AllowHttp, useTLS)
+	err = p.saveForwarder(clientAddr, lease, request)
 	if err != nil {
 		leasor.Unregister(lease)
 		return nil, err
@@ -100,15 +98,58 @@ func (p *httpProxy) Register(
 // http request paths. This is needed for third party applications that expect
 // to get requests for / not /pattern/
 func (p *httpProxy) saveForwarder(clientAddr string, lease *gate.Lease,
-	stripPattern, allowHTTP, useTLS bool) error {
+	request *gate.RegisterRequest) error {
 
 	var protocol string
-	if useTLS {
+	addrPort := fmt.Sprintf("%v:%v", clientAddr, lease.Port)
+
+	// TODO: when assimilate supports writing cert files, maybe make it so
+	// normally we only accept the portal root CA (and maybe add the system root
+	// CAs as well) and only skip verify if the hostname field was set.
+	//
+	// Then if people can run assimilate on the same machine as their server
+	// they'll have to use the protal signed cert so that we can actually verify
+	// it instead of turning that off.
+	var conf *tls.Config
+	if len(request.CertificateRequest) == 0 {
+		conf = &tls.Config{
+			InsecureSkipVerify: true,
+		}
+	} else {
+		roots := p.state.RootCAs()
+		// Note: We can use conf.VerifyPeerCertificate to only allow talking to
+		// client that we registered with. This is pointless right now because we
+		// allow any client with a valid token (same as the clients that have a
+		// valid cert signed by us) to replace any lease. We assume validated
+		// clients are not malicious right now.
+		conf = &tls.Config{
+			RootCAs:   roots,
+			ClientCAs: roots,
+			// Use the server cert for client auth
+			GetClientCertificate: func(*tls.CertificateRequestInfo) (*tls.Certificate, error) {
+				return p.rootCert.GetCertificate(nil)
+			},
+		}
+	}
+
+	// Detect TLS support
+	conn, err := tls.Dial("tcp", addrPort, conf)
+	if err == nil {
+		conn.Close()
 		protocol = "https://"
 	} else {
-		protocol = "http://"
+		if len(request.CertificateRequest) != 0 {
+			log.Printf("Error: failed to connect using TLS for the %v backend. When certificate_request is used, you must serve with that certificate. Message: %v",
+				lease.Pattern, err)
+			protocol = "https://"
+		} else {
+			log.Printf("Warning: TLS is not supported for the %v backend. This internal traffic will not be encrypted. Message: %v",
+				lease.Pattern, err)
+			protocol = "http://"
+		}
 	}
-	backend, err := url.Parse(protocol + clientAddr + ":" + strconv.Itoa(int(lease.Port)))
+
+	backend, err := url.Parse(protocol + addrPort)
 	if err != nil {
 		return err
 	}
@@ -119,22 +160,7 @@ func (p *httpProxy) saveForwarder(clientAddr string, lease *gate.Lease,
 	// Accept certificates signed by the latest portal cert
 	transport := http.DefaultTransport.(*http.Transport).Clone()
 	transport.DialTLSContext = func(ctx context.Context, network, addr string) (net.Conn, error) {
-		roots := p.state.RootCAs()
-		dialConf := &tls.Config{
-			RootCAs:   roots,
-			ClientCAs: roots,
-			// Use the server cert for client auth
-			GetClientCertificate: func(*tls.CertificateRequestInfo) (*tls.Certificate, error) {
-				return p.rootCert.GetCertificate(nil)
-			},
-		}
-
-		// Note: We can use dialConf.VerifyPeerCertificate to only allow talking to
-		// client that we registered with. This is pointless right now because we
-		// allow any client with a valid token (same as the clients that have a
-		// valid cert signed by us) to replace any lease. We assume validated
-		// clients are not malicious right now.
-		dialer := &tls.Dialer{Config: dialConf}
+		dialer := &tls.Dialer{Config: conf}
 		return dialer.DialContext(ctx, network, addr)
 	}
 
@@ -151,7 +177,7 @@ func (p *httpProxy) saveForwarder(clientAddr string, lease *gate.Lease,
 			if req.URL.Path[0] != '/' {
 				req.URL.Path = "/" + req.URL.Path
 			}
-			if stripPattern {
+			if request.StripPattern {
 				if pattern[len(pattern)-1] != '/' { // if the pattern doesn't end in / then it's exact match only
 					req.URL.Path = "/"
 				} else {
@@ -182,7 +208,7 @@ func (p *httpProxy) saveForwarder(clientAddr string, lease *gate.Lease,
 	fwd := &forwarder{
 		Handler:   proxy,
 		Lease:     lease,
-		AllowHTTP: allowHTTP,
+		AllowHTTP: request.AllowHttp,
 	}
 	p.forwarders.Store(pattern, fwd)
 	return nil

portal/embedportal/tcp_proxyserv.go
@@ -73,8 +73,14 @@ func (p *tcpProxy) Register(clientAddr string, request *gate.RegisterRequest) (*
 	return lease, nil
 }
 
-func handleConnection(publicConn net.Conn, serverAddress string, quit chan struct{}) {
-	privateConn, err := net.Dial("tcp", serverAddress)
+func handleConnection(publicConn net.Conn, serverAddress string, tlsConf *tls.Config, quit chan struct{}) {
+	var privateConn net.Conn
+	var err error
+	if tlsConf != nil {
+		privateConn, err = tls.Dial("tcp", serverAddress, tlsConf)
+	} else {
+		privateConn, err = net.Dial("tcp", serverAddress)
+	}
 	if err != nil {
 		log.Printf("Failed to connect to TCP Proxy backend (for client %v): %v",
 			publicConn.RemoteAddr(), err)
@@ -116,6 +122,23 @@ func startTCPForward(tlsListener net.Listener, serverAddress string, quit chan s
 		<-quit // stop listening when we quit
 		tlsListener.Close()
 	}()
+
+	// TODO: when assimilate supports certificate requests, make it so we
+	// verify using the portal root CA (and maybe system CAs) when the cert
+	// request was used.
+	conf := &tls.Config{
+		InsecureSkipVerify: true,
+	}
+	// Detect TLS support
+	conn, err := tls.Dial("tcp", serverAddress, conf)
+	if err == nil {
+		conn.Close()
+	} else {
+		log.Printf("Warning: TLS is not supported for the %v TCP backend. This internal traffic will not be encrypted. Message: %v",
+			serverAddress, err)
+		conf = nil
+	}
+
 	go func() {
 		for {
 			publicConn, err := tlsListener.Accept()
@@ -124,9 +147,10 @@ func startTCPForward(tlsListener net.Listener, serverAddress string, quit chan s
 					serverAddress, err)
 				break
 			}
+
 			// Use a goroutine just to not wait until the Dial is done before we
 			// can accept connections again
-			go handleConnection(publicConn, serverAddress, quit)
+			go handleConnection(publicConn, serverAddress, conf, quit)
 		}
 		tlsListener.Close()
 	}()

portal/gate/service.pb.go
@@ -96,6 +96,11 @@ type RegisterRequest struct {
 	// rule for, but if you have a cpanel-only host or otherwise don't have access
 	// to run arbitrary code then you can use this setting to run assimilate on
 	// another machine.
+	//
+	// If you use this you need to be mindful of TLS and the network you're
+	// sending the data over. Ideally you should set up a self-signed certificate
+	// on the other machine and portal will detect TLS support. Otherwise make
+	// sure you only use this with a trusted local network.
 	Hostname string `protobuf:"bytes,6,opt,name=hostname,proto3" json:"hostname,omitempty"`
 	// If true, remove the pattern in the URL of HTTP requests we forward to the
 	// backend to hide that it is behind a reverse proxy.

portal/gate/service.proto
@@ -42,6 +42,11 @@ message RegisterRequest {
 	// rule for, but if you have a cpanel-only host or otherwise don't have access
 	// to run arbitrary code then you can use this setting to run assimilate on
 	// another machine.
+	//
+	// If you use this you need to be mindful of TLS and the network you're
+	// sending the data over. Ideally you should set up a self-signed certificate
+	// on the other machine and portal will detect TLS support. Otherwise make
+	// sure you only use this with a trusted local network.
 	string hostname = 6;
 
 	// If true, remove the pattern in the URL of HTTP requests we forward to the

commit ad4d2fb06f5e96108bb180decf415cf1a2a1e796

Fix http proxy cert verification when using req.CertificateRequest
When you request portal to sign a tls cert for you for http proxies, it
then verifies that you actually used that cert so no one can
man-in-the-middle the http proxy by crashing your server and running
their own on your leased port.

When I implemented this I forgot that x509.CertPool is not thread safe
and therefore does not get automatically updated when the portal root CA
cert expires (after 10 * leaseTTL or 10 days). So after 10 days when the
daemon-native http proxy clients like host or the dashboard renew their
lease they will get a cert signed by the new root CA which is not in the
list for the tls.Config saved in the registered forwarder for that path.

This bug was introduced in cb6357666d30f2b31639a1d9274fbd0e9708f9d0
(published in v0.6.2) when I moved the code out of the lambda because I
added an option to change the config. Now we add back in updating
RootCAs on every connection.

portal/embedportal/http_proxyserv.go
@@ -123,8 +123,7 @@ func (p *httpProxy) saveForwarder(clientAddr string, lease *gate.Lease,
 		// valid cert signed by us) to replace any lease. We assume validated
 		// clients are not malicious right now.
 		conf = &tls.Config{
-			RootCAs:   roots,
-			ClientCAs: roots,
+			RootCAs: roots,
 			// Use the server cert for client auth
 			GetClientCertificate: func(*tls.CertificateRequestInfo) (*tls.Certificate, error) {
 				return p.rootCert.GetCertificate(nil)
@@ -160,7 +159,12 @@ func (p *httpProxy) saveForwarder(clientAddr string, lease *gate.Lease,
 	// Accept certificates signed by the latest portal cert
 	transport := http.DefaultTransport.(*http.Transport).Clone()
 	transport.DialTLSContext = func(ctx context.Context, network, addr string) (net.Conn, error) {
-		dialer := &tls.Dialer{Config: conf}
+		usedConf := conf
+		if conf.RootCAs != nil {
+			usedConf = conf.Clone()
+			usedConf.RootCAs = p.state.RootCAs()
+		}
+		dialer := &tls.Dialer{Config: usedConf}
 		return dialer.DialContext(ctx, network, addr)
 	}
 

portal/embedportal/state.go
@@ -19,8 +19,13 @@ type stateManager struct {
 
 	registrations map[uint32]*Registration // lease port key
 
-	rootCAs      map[*x509.Certificate]struct{}
-	mutCertPool  *x509.CertPool
+	// Store the rootCAs separately so we can write them to a file, because
+	// unfortunately CertPool has no non-depracted methods to get the certs back.
+	rootCAs map[*x509.Certificate]struct{}
+	// This is what tls.Config expects, so we have to maintain this class
+	// Unfortunately it is not thread safe
+	mutCertPool *x509.CertPool
+	// This is how we make it thread safe, we store clones of the CertPool here
 	readCertPool *atomic.Value
 
 	token *atomic.Value