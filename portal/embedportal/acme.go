fsmv/daemon

commit dbaa9e2b7e80a0cb8f4029b5dca8cae7ec9feaa1

Save and restore acme auto cert state!
 - Refactored how state file loading works. Now stateManager actually
   loads the state file instead of the rpc server. The rpc server now
   only registers the loaded state.
   - This was needed because as state was loaded we were also saving the
     file for each item loaded. This caused a problem where retrieving
     the acme certs overwrote all the registrations and the state was
     lost.
   - It was a weird anyway having rpc server do the loading I'm glad I
     finally did this refactor
 - Refactored the tls_cert.go functions so that there are more distinct
   code paths for the file certs and the acme certs. This cleaned up the
   weirdness where originally I needed a sync.Once in the certGenerator
   and we don't have the dance of closing files so they can be reopened
   by the "initial refresh" of the cert refresher.
 - Renamed several functions in stateManager to be Save instead of New
 - Cleaned up and commented the flow of the portal.go main function and
   renamed the tcp server function to reflect what it does better

portal/embedportal/acme.go
@@ -41,16 +41,19 @@ func acmeClient(accountKey crypto.Signer) *acme.Client {
 	}
 }
 
+// Make an account
+// TODO: should I bother adding the email address here?
 func fetchACMEAccount(client *acme.Client) (*acme.Account, error) {
 	ctx := context.Background()
 	account, err := client.GetReg(ctx, "")
-	if errors.Is(err, acme.ErrNoAccount) {
-		account, err = client.Register(ctx, &acme.Account{}, acme.AcceptTOS)
-		if err != nil {
-			err = fmt.Errorf("acme.Register error: %w", err)
+	if err != nil {
+		if errors.Is(err, acme.ErrNoAccount) {
+			account, err = client.Register(ctx, &acme.Account{}, acme.AcceptTOS)
+			if err != nil {
+				err = fmt.Errorf("acme.Register error: %w", err)
+			}
 		}
-	} else {
-		err = fmt.Errorf("acme.GetReg error: %w", err)
+		return nil, fmt.Errorf("acme.GetReg error: %w", err)
 	}
 	if err != nil {
 		return nil, err
@@ -64,7 +67,6 @@ func obtainACMECert(domain string, client *acme.Client, account *acme.Account, c
 	if err != nil {
 		return nil, fmt.Errorf("acme.Discover error: %w", err)
 	}
-	// Make an account (can add email here)
 	log.Printf("Registering a certificate for %v with: %v", domain, directory.Website)
 	log.Print("By using automatic certs you agree to the CA's TOS: ", directory.Terms)
 	if len(directory.CAA) > 0 {

portal/embedportal/portal.go
@@ -58,18 +58,18 @@ func Run(flags *flag.FlagSet, args []string) {
 	tlsCertSpec := flags.String("tls_cert", "", ""+
 		"The filepath to the tls cert file (fullchain.pem).\n"+
 		"Accepts multiple certificates with a comma separated list.\n"+
+		"Files are automatically re-read when portal receives SIGUSR1\n"+
+		"or 2/3 of the expiration date.\n"+
 		"This is not needed with spawn because it uses the SPAWN_FILES env var.")
 	tlsKeySpec := flags.String("tls_key", "", ""+
 		"The filepath to the tls key file (privkey.pem).\n"+
 		"Accepts multiple keys with a comma separated list.\n"+
+		"Files are automatically re-read when portal receives SIGUSR1\n"+
+		"or 2/3 of the expiration date.\n"+
 		"This is not needed with spawn because it uses the SPAWN_FILES env var.")
-	// TODO: maybe just delete this flag
-	autoTLSCerts := flags.Bool("auto_tls_certs", true, ""+
-		"If true update the tls files when SIGUSR1 is received. The\n"+
-		"-tls_cert and -tls_key paths must either both be file paths or both be\n"+
-		"OS pipe fd numbers produced by the auto_tls_certs spawn config option.\n")
-	// TODO: default to empty string eventually? Might break people's configs if
-	// they're not using autocert_domains yet.
+	// TODO: default to empty string eventually?
+	// I want to so that we don't make a folder we don't need, but that might
+	// break people's configs if they're not using autocert_domains yet.
 	certChallengeWebRoot := flags.String("cert_challenge_webroot", "./cert-challenge/", ""+
 		"Set to a local folder path to enable hosting the let's encrypt webroot\n"+
 		"challenge path ("+certChallengePattern+") so you can auto-renew with\n"+
@@ -97,20 +97,29 @@ func Run(flags *flag.FlagSet, args []string) {
 	}
 
 	state := newStateManager(*saveFilepath)
+	if err := state.Load(); err != nil {
+		log.Print("Failed to load state: ", err)
+	} else {
+		log.Print("Successfully loaded state.")
+	}
+
+	// Set up the new CA root cert for signing API client TLS certs
 	onCertRenew := func(cert *tls.Certificate) {
-		if err := state.NewRootCA(cert.Certificate[0]); err != nil {
+		if err := state.SaveRootCA(cert.Certificate[0]); err != nil {
 			log.Print("Error saving new root CA, new backend connections may not work: ", err)
 		} else {
 			log.Print("Renewed root CA cert.")
 		}
 	}
-	challenges := &acmeChallenges{}
-	leasor := makeClientLeasor(uint16(*portRangeStart), uint16(*portRangeEnd), reservedPorts, quit)
 	rootCert, err := tools.AutorenewSelfSignedCertificate("portal",
 		10*leaseTTL, true /*isCA*/, onCertRenew, quit)
 	if err != nil {
 		log.Fatalf("Failed to create a self signed certificate for the RPC server: %v", err)
 	}
+
+	challenges := &acmeChallenges{}
+	leasor := makeClientLeasor(uint16(*portRangeStart), uint16(*portRangeEnd), reservedPorts, quit)
+
 	httpProxy, err := makeHTTPProxy(leasor, rootCert,
 		httpListener, httpsListener,
 		*defaultHost, challenges, *certChallengeWebRoot,
@@ -118,49 +127,44 @@ func Run(flags *flag.FlagSet, args []string) {
 	if err != nil {
 		log.Fatalf("Failed to start HTTP proxy server: %v", err)
 	}
+	// Start HTTP first for cert challenges
 	httpProxy.StartHTTP(quit)
 	log.Print("Started HTTP proxy server")
 
+	// Load the serving TLS certs.
+	// This may cause acme cert challenges over HTTP.
 	serveCert, err := loadTLSConfig(
 		strings.Split(*tlsCertSpec, ","),
 		strings.Split(*tlsKeySpec, ","),
-		domains, *autoTLSCerts, challenges, quit)
+		domains, challenges, state, quit)
 	if err != nil {
 		log.Fatalf("failed to load TLS config: %v", err)
 	}
 
-	// Load the previous save data from the file before we overwrite it
-	var saveData []byte
-	if *saveFilepath != "" {
-		saveData, err = os.ReadFile(*saveFilepath)
-		if err != nil {
-			log.Print("No save data: ", err)
-		}
-	}
+	// Doesn't actually do anything until there are registrations (there are no
+	// ports to open if clients haven't requested any)
+	tcpProxy := makeTCPProxy(leasor, serveCert, quit)
 
-	tcpProxy := startTCPProxy(leasor, serveCert, quit)
-
-	// Note: State file gets loaded here (because only the RPC server knows how to
-	// register both the TCP and HTTP proxies, and the state stores RPC requests)
-	//
-	// This needs to go last because it prints the token string spawn looks for
+	// Starts serving the rpc server port.
+	// First loads the registrations from the state into the two proxy servers.
 	_, err = startRPCServer(leasor,
 		tcpProxy, httpProxy, uint16(*rpcPort),
-		rootCert, saveData, state, quit)
+		rootCert, state, quit)
 	log.Print("Started rpc server on port ", *rpcPort)
 	if err != nil {
 		log.Fatal("Failed to start RPC server:", err)
 	}
 
+	// Wait until after we have loaded the registrations so we don't serve a bunch
+	// of 404s during startup
 	httpProxy.StartHTTPS(serveCert, quit)
 	log.Print("Started HTTPS proxy server")
 
-	// TODO: finish auto certs
-	//  + Flag to set which domains to get certs for
-	//  + Use the certs in the https server
-	//  + Autorenew the certs
-	//  - Save and load the accountKey and the certs
-	//  - Always run the challenge handler even if there's no webroot
+	// Spawn looks for this string to know when portal has started. So we need to
+	// have the API port listening before we print this.
+	//
+	// If this changes you have to update the string in spawn so it can find it
+	log.Printf("**** Portal API token: %v ****", state.Token())
 
 	<-quit // Wait for quit
 }

portal/embedportal/http_proxyserv.go
@@ -411,18 +411,21 @@ func runServer(quit chan struct{}, name string,
 }
 
 func makeChallengeHandler(webRoot string, challenges *acmeChallenges) (http.Handler, error) {
-	if err := os.MkdirAll(webRoot, 0775); err != nil {
-		return nil, err
+	var fileServer http.Handler
+	if webRoot != "" {
+		if err := os.MkdirAll(webRoot, 0775); err != nil {
+			return nil, err
+		}
+		dir := tools.SecureHTTPDir{
+			Dir:                   http.Dir(webRoot),
+			AllowDotfiles:         true,
+			AllowDirectoryListing: false,
+		}
+		if err := dir.TestOpen("/"); err != nil {
+			return nil, err
+		}
+		fileServer = http.FileServer(dir)
 	}
-	dir := tools.SecureHTTPDir{
-		Dir:                   http.Dir(webRoot),
-		AllowDotfiles:         true,
-		AllowDirectoryListing: false,
-	}
-	if err := dir.TestOpen("/"); err != nil {
-		return nil, err
-	}
-	fileServer := http.FileServer(dir)
 	return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
 		log.Printf("%v requested %v%v (useragent: %q)",
 			req.RemoteAddr, req.Host, req.URL.EscapedPath(), req.UserAgent())
@@ -431,7 +434,11 @@ func makeChallengeHandler(webRoot string, challenges *acmeChallenges) (http.Hand
 		if ok {
 			w.Write([]byte(resp))
 		} else {
-			fileServer.ServeHTTP(w, req)
+			if fileServer != nil {
+				fileServer.ServeHTTP(w, req)
+			} else {
+				http.NotFound(w, req)
+			}
 		}
 	}), nil
 }
@@ -451,20 +458,18 @@ func makeHTTPProxy(l *clientLeasor, rootCert *tls.Config,
 	l.OnCancel(ret.Unregister)
 
 	// Set up serving cert challenges
-	if certChallengeWebRoot != "" {
-		handler, err := makeChallengeHandler(certChallengeWebRoot, ret.challenges)
-		if err != nil {
-			log.Print("Failed to start cert challenge webroot: ", err)
-		} else {
-			ret.forwarders.Store(certChallengePattern, &forwarder{
-				Handler: handler,
-				Lease: &gate.Lease{
-					Pattern: certChallengePattern,
-				},
-				AllowHTTP: true,
-			})
-			log.Print("Started serving cert challenge path.")
-		}
+	handler, err := makeChallengeHandler(certChallengeWebRoot, ret.challenges)
+	if err != nil {
+		log.Print("Failed to start cert challenge HTTP handler: ", err)
+	} else {
+		ret.forwarders.Store(certChallengePattern, &forwarder{
+			Handler: handler,
+			Lease: &gate.Lease{
+				Pattern: certChallengePattern,
+			},
+			AllowHTTP: true,
+		})
+		log.Print("Started serving cert challenges.")
 	}
 
 	// Close the servers on quit signal

portal/embedportal/rpcserv.go
@@ -21,7 +21,6 @@ import (
 	"google.golang.org/grpc/metadata"
 	"google.golang.org/grpc/peer"
 	"google.golang.org/grpc/status"
-	"google.golang.org/protobuf/proto"
 	"google.golang.org/protobuf/types/known/emptypb"
 	"google.golang.org/protobuf/types/known/timestamppb"
 )
@@ -38,30 +37,11 @@ type rpcServ struct {
 	quit         chan struct{}
 }
 
-func (s *rpcServ) loadState(saveData []byte) {
-	state := &State{}
-	if err := proto.Unmarshal(saveData, state); err != nil {
-		log.Print("Failed to unmarshal save state file: ", err)
-		return
-	}
-
-	s.state.SetToken(state.ApiToken) // generates a new token if not set
-
+func (s *rpcServ) loadRegistrations() {
+	attempted := 0
 	loaded := 0
-	for _, ca := range state.RootCAs {
-		if err := s.state.NewRootCA(ca); err != nil {
-			log.Print("Failed to save root CA: ", err)
-			continue
-		}
-		loaded += 1
-	}
-	if len(state.RootCAs) > 0 {
-		log.Printf("Successfully loaded %v/%v saved root CAs", loaded, len(state.RootCAs))
-	}
-
-	// Register the saved registrations
-	loaded = 0
-	for _, registration := range state.Registrations {
+	s.state.ForEachRegistration(func(registration *Registration) {
+		attempted += 1
 		if registration.Request.FixedPort == 0 {
 			// Request a new lease for the port we had before if the original request
 			// was for a random port
@@ -80,12 +60,12 @@ func (s *rpcServ) loadState(saveData []byte) {
 		_, err := s.internalRegister(registration.Lease.Address, registration.Request, timeoutTime)
 		if err != nil {
 			log.Printf("Failed to recreate registration: %v\n%v", err, registration)
-			continue
+			return
 		}
 		loaded += 1
-	}
-	if len(state.Registrations) > 0 {
-		log.Printf("Successfully loaded %v/%v saved registrations", loaded, len(state.Registrations))
+	})
+	if attempted > 0 {
+		log.Printf("Successfully loaded %v/%v saved registrations", loaded, attempted)
 	}
 }
 
@@ -134,6 +114,12 @@ func (s *rpcServ) Register(ctx context.Context, request *gate.RegisterRequest) (
 		log.Printf("Registration failed (%v), failed to setup proxy: %v", request.Pattern, err)
 		return nil, err
 	}
+	if err = s.state.SaveRegistration(&Registration{
+		Request: request,
+		Lease:   lease,
+	}); err != nil {
+		return nil, err
+	}
 	// TODO: should the cert code just go in port_leasor?
 	//
 	// Currently the cert doesn't get saved in the state which is kind of weird.
@@ -153,15 +139,6 @@ func (s *rpcServ) internalRegister(clientAddr string, request *gate.RegisterRequ
 		lease, err = s.tcpProxy.Register(clientAddr, request, fixedTimeout)
 	} else {
 		lease, err = s.httpProxy.Register(clientAddr, request, fixedTimeout)
-	}
-	if err != nil {
-		return
-	}
-	if err = s.state.NewRegistration(&Registration{
-		Request: request,
-		Lease:   lease,
-	}); err != nil {
-		return nil, err
 	}
 	return
 }
@@ -244,8 +221,7 @@ func (s *rpcServ) Renew(ctx context.Context, lease *gate.Lease) (*gate.Lease, er
 func startRPCServer(clientLeasor *clientLeasor,
 	tcpProxy *tcpProxy, httpProxy *httpProxy,
 	port uint16, rootCert *tls.Config,
-	saveData []byte, state *stateManager,
-	quit chan struct{}) (*rpcServ, error) {
+	state *stateManager, quit chan struct{}) (*rpcServ, error) {
 
 	s := &rpcServ{
 		clientLeasor: clientLeasor,
@@ -256,7 +232,7 @@ func startRPCServer(clientLeasor *clientLeasor,
 		rootCert:     rootCert,
 	}
 	clientLeasor.OnCancel(s.state.Unregister)
-	s.loadState(saveData)
+	s.loadRegistrations()
 	server := grpc.NewServer(
 		// TODO: Have a flag like -internet_accessable_rpc which makes the RPC
 		// server use the web server cert, and make the portal client library verify
@@ -287,11 +263,6 @@ func startRPCServer(clientLeasor *clientLeasor,
 	if err != nil {
 		return nil, fmt.Errorf("Failed to start listener: %v", err)
 	}
-	// Spawn looks for this string to know when portal has started. So we need to
-	// have the API port listening before we print this.
-	//
-	// If this changes you have to update the string in spawn so it can find it
-	log.Printf("**** Portal API token: %v ****", s.state.Token())
 	go func() {
 		server.Serve(l) // logs any errors itself instead of returning
 		log.Print("RPC server died, quitting")

portal/embedportal/state.go
@@ -1,10 +1,13 @@
 package embedportal
 
 import (
+	"crypto"
+	"crypto/tls"
 	"crypto/x509"
 	"fmt"
 	"log"
 	"os"
+	"sort"
 	"sync"
 	"sync/atomic"
 	"time"
@@ -13,6 +16,10 @@ import (
 	"ask.systems/daemon/tools"
 	"google.golang.org/protobuf/proto"
 )
+
+func leaseKey(lease *gate.Lease) string {
+	return fmt.Sprintf("%s:%d:%s", lease.Address, lease.Port, lease.Pattern)
+}
 
 type stateManager struct {
 	mut          *sync.Mutex
@@ -30,6 +37,9 @@ type stateManager struct {
 	readCertPool *atomic.Value
 
 	token *atomic.Value
+
+	acmeAccount  crypto.Signer
+	certificates map[string]*tls.Certificate // domain name key
 }
 
 func newStateManager(saveFilepath string) *stateManager {
@@ -43,12 +53,79 @@ func newStateManager(saveFilepath string) *stateManager {
 		mutCertPool:  x509.NewCertPool(),
 		readCertPool: &atomic.Value{},
 
-		token: &atomic.Value{},
+		token:        &atomic.Value{},
+		certificates: make(map[string]*tls.Certificate),
 	}
 	if s.saveFilepath == "" {
 		log.Print("No save file, state is only saved in memory.")
 	}
 	return s
+}
+
+func (s *stateManager) Load() error {
+	// Unmarshal the proto
+	if s.saveFilepath == "" {
+		return nil
+	}
+	saveData, err := os.ReadFile(s.saveFilepath)
+	if err != nil {
+		return fmt.Errorf("No save data: %w", err)
+	}
+	state := &State{}
+	if err := proto.Unmarshal(saveData, state); err != nil {
+		return fmt.Errorf("Failed to unmarshal save state file: %w", err)
+	}
+	s.mut.Lock()
+	defer s.mut.Unlock()
+
+	// Load the registrations
+	for i, r := range state.Registrations {
+		if err := s.unsafeSaveRegistration(r); err != nil {
+			return fmt.Errorf("Error saving registration %v for %v: %w",
+				i, r.GetRequest().GetPattern(), err)
+		}
+	}
+
+	// Load the root CA certs
+	loaded := 0
+	for i, ca := range state.RootCAs {
+		if err := s.unsafeSaveRootCA(ca); err != nil {
+			return fmt.Errorf("Failed to save root CA #%v: %w", i, err)
+		}
+		loaded += 1
+	}
+
+	// Load the token
+	if state.ApiToken == "" {
+		state.ApiToken = tools.RandomString(32)
+	}
+	s.token.Store(state.ApiToken)
+
+	// Load acme account key
+	if len(state.AcmeAccount) > 0 {
+		accountKeyAny, err := x509.ParsePKCS8PrivateKey(state.AcmeAccount)
+		accountKey, ok := accountKeyAny.(crypto.Signer)
+		if err != nil || !ok {
+			return fmt.Errorf("Failed to load acme account key: %w", err)
+		}
+		s.acmeAccount = accountKey
+	}
+
+	// Load the acme auto certs
+	for _, cert := range state.Certificates {
+		certKeyAny, err := x509.ParsePKCS8PrivateKey(cert.Key)
+		certKey, ok := certKeyAny.(crypto.Signer)
+		if err != nil || !ok {
+			return fmt.Errorf("Failed to load key for domain %v: %w", cert.Domain, err)
+		}
+		tlsCert, err := tools.TLSCertificateFromBytes(cert.Der, certKey)
+		if err != nil {
+			return fmt.Errorf("TLS cert for domain %v not valid: %w", cert.Domain, err)
+		}
+		s.certificates[cert.Domain] = tlsCert
+	}
+
+	return nil
 }
 
 func writeFileSync(name string, data []byte, perm os.FileMode) error {
@@ -72,9 +149,6 @@ func (s *stateManager) saveUnsafe() {
 	}
 	// Build the save state proto from the current in memory state
 	state := &State{}
-	if token := s.token.Load(); token != nil {
-		state.ApiToken = token.(string)
-	}
 
 	for _, r := range s.registrations {
 		state.Registrations = append(state.Registrations, r)
@@ -87,6 +161,38 @@ func (s *stateManager) saveUnsafe() {
 		}
 		state.RootCAs = append(state.RootCAs, ca.Raw)
 	}
+
+	if token := s.token.Load(); token != nil {
+		state.ApiToken = token.(string)
+	}
+
+	// TODO: better error handling in this function
+	// Save the ACME Account
+	if s.acmeAccount != nil {
+		if accountBytes, err := x509.MarshalPKCS8PrivateKey(s.acmeAccount); err != nil {
+			log.Print("Failed to marshal acme account key: ", err)
+		} else {
+			state.AcmeAccount = accountBytes
+		}
+	}
+
+	// Save all of the certficates per domain
+	for domain, cert := range s.certificates {
+		keyBytes, err := x509.MarshalPKCS8PrivateKey(cert.PrivateKey)
+		if err != nil {
+			log.Printf("Failed to marshal the cert key for %v: %v", domain, err)
+			continue
+		}
+		state.Certificates = append(state.Certificates,
+			&Certificate{
+				Domain: domain,
+				Der:    cert.Certificate,
+				Key:    keyBytes,
+			})
+	}
+	sort.Slice(state.Certificates, func(i, j int) bool {
+		return state.Certificates[i].Domain < state.Certificates[j].Domain
+	})
 
 	saveData, err := proto.Marshal(state)
 	if err != nil {
@@ -108,30 +214,26 @@ func (s *stateManager) Token() string {
 	return s.token.Load().(string)
 }
 
-// If empty, generate a new token
-func (s *stateManager) SetToken(token string) {
-	if token == "" {
-		token = tools.RandomString(32)
-	}
-
-	s.mut.Lock()
-	defer s.mut.Unlock()
-	s.token.Store(token)
-	s.saveUnsafe()
-}
-
-func (s *stateManager) NewRootCA(rawCert []byte) error {
+func (s *stateManager) unsafeSaveRootCA(rawCert []byte) error {
 	newRoot, err := x509.ParseCertificate(rawCert)
 	if err != nil {
 		return err
 	}
 
-	s.mut.Lock()
-	defer s.mut.Unlock()
-
 	s.rootCAs[newRoot] = struct{}{}
 	s.mutCertPool.AddCert(newRoot)
 	s.readCertPool.Store(s.mutCertPool.Clone())
+	return nil
+}
+
+func (s *stateManager) SaveRootCA(rawCert []byte) error {
+	s.mut.Lock()
+	defer s.mut.Unlock()
+
+	if err := s.unsafeSaveRootCA(rawCert); err != nil {
+		return err
+	}
+
 	s.saveUnsafe()
 	return nil
 }
@@ -140,8 +242,12 @@ func (s *stateManager) RootCAs() *x509.CertPool {
 	return s.readCertPool.Load().(*x509.CertPool)
 }
 
-func leaseKey(lease *gate.Lease) string {
-	return fmt.Sprintf("%s:%d:%s", lease.Address, lease.Port, lease.Pattern)
+func (s *stateManager) ForEachRegistration(body func(*Registration)) {
+	s.mut.Lock()
+	defer s.mut.Unlock()
+	for _, r := range s.registrations {
+		body(r)
+	}
 }
 
 func (s *stateManager) LookupRegistration(lease *gate.Lease) *Registration {
@@ -151,10 +257,7 @@ func (s *stateManager) LookupRegistration(lease *gate.Lease) *Registration {
 	return s.registrations[leaseKey(lease)]
 }
 
-func (s *stateManager) NewRegistration(r *Registration) error {
-	s.mut.Lock()
-	defer s.mut.Unlock()
-
+func (s *stateManager) unsafeSaveRegistration(r *Registration) error {
 	key := leaseKey(r.Lease)
 
 	if _, ok := s.registrations[key]; ok {
@@ -162,6 +265,15 @@ func (s *stateManager) NewRegistration(r *Registration) error {
 	}
 
 	s.registrations[key] = r
+	return nil
+}
+
+func (s *stateManager) SaveRegistration(r *Registration) error {
+	s.mut.Lock()
+	defer s.mut.Unlock()
+	if err := s.unsafeSaveRegistration(r); err != nil {
+		return err
+	}
 	s.saveUnsafe()
 	return nil
 }
@@ -188,4 +300,41 @@ func (s *stateManager) Unregister(oldLease *gate.Lease) {
 	delete(s.registrations, leaseKey(oldLease))
 
 	s.saveUnsafe()
+}
+
+func (s *stateManager) TLSCert(domain string) *tls.Certificate {
+	s.mut.Lock()
+	defer s.mut.Unlock()
+
+	cert, ok := s.certificates[domain]
+	if !ok {
+		return nil
+	}
+	return cert
+}
+
+func (s *stateManager) SaveTLSCert(domain string, cert *tls.Certificate) error {
+	s.mut.Lock()
+	defer s.mut.Unlock()
+
+	s.certificates[domain] = cert
+
+	s.saveUnsafe()
+	return nil
+}
+
+func (s *stateManager) ACMEAccount() crypto.Signer {
+	s.mut.Lock()
+	defer s.mut.Unlock()
+	return s.acmeAccount
+}
+
+func (s *stateManager) SaveACMEAccount(accountKey crypto.Signer) error {
+	s.mut.Lock()
+	defer s.mut.Unlock()
+
+	s.acmeAccount = accountKey
+
+	s.saveUnsafe()
+	return nil
 }

commit c8126629e190bd285a9af223409e9cd238fc03c9

Add a github actions test for the new acme autocerts portal feature
 - Added a flag to set the ACME server. This is useful anyway for people
   that want to use alt providers or private servers, and I needed it
   for testing.
 - Also added an env var to set a CA for the ACME server. Annoyingly I
   couldn't avoid this by just using http for testing since pebble
   insists on HTTPS. I thought about making it a flag but I really don't
   want people to use this option in prod, it should just be the system
   CA roots.
 - Fixed a bug with the ACME account registration error logic that I
   introduced in dbaa9e2b7e80a0cb8f4029b5dca8cae7ec9feaa1 I'm a bit
   surprised, I must have had a saved account already from development
   so I didn't run into this on my home server. I guess this just got
   confused and buried in the refactoring.

.github/workflows/tests.yml
@@ -7,7 +7,7 @@ on:
     branches: [ "main" ]
 
 jobs:
-  test-demo:
+  tests-and-demo:
     runs-on: ${{ matrix.os }}
 
     strategy:
@@ -22,13 +22,13 @@ jobs:
       with:
         go-version-file: go.mod
 
-    - name: Build
+    - name: Build daemon
       run: go build -v ./...
 
-    - name: Test
+    - name: Run daemon unit tests
       run: go test -v ./...
 
-    - name: Quickstart
+    - name: Test demo mode
       shell: bash
       run: |
         function endtest () {
@@ -46,6 +46,79 @@ jobs:
         if [ -z "$(grep 'Token received' logs.txt)" ]; then exit 2; fi
         if [ -z "$(grep 'Obtained TLS lease' logs.txt)" ]; then exit 3; fi
         if [ -z "$(grep 'https://127.0.0.1:8080/daemon/' logs.txt)" ]; then exit 4; fi
+
+  test-auto-certs:
+    runs-on: ubuntu-latest
+
+    steps:
+    - name: Clone daemon
+      uses: actions/checkout@v4
+      with:
+        path: 'daemon'
+
+    - name: Clone Pebble ACME Server
+      uses: actions/checkout@v4
+      with:
+        repository: 'letsencrypt/pebble'
+        ref: 'v2.7.0'
+        path: 'pebble'
+
+    - name: Set up Go
+      uses: actions/setup-go@v5
+      with:
+        go-version-file: pebble/go.mod
+        cache-dependency-path: |
+          daemon/go.sum
+          pebble/go.sum
+
+    - name: Install portal
+      run: |
+        cd ./daemon/portal
+        go install -v
+
+    - name: Install Pebble ACME Server
+      run: |
+        cd ./pebble
+        go install -v ./cmd/...
+        echo "127.0.0.1 one.example" | sudo tee -a /etc/hosts
+        echo "127.0.0.1 two.example" | sudo tee -a /etc/hosts
+
+    - name: Test portal autocerts
+      run: |
+        cd ./pebble
+        PEBBLE_WFE_NONCEREJECT=0 PEBBLE_AUTHZREUSE=0 pebble &
+        cd ..
+
+        sleep 5
+
+        ACME_SERVER_CERT=./pebble/test/certs/pebble.minica.pem portal \
+          -autocert_domains=one.example,two.example \
+          -autocert_server=https://127.0.0.1:14000/dir \
+          -http_port=5002 \
+          -https_port=5001 \
+          -cert_challenge_webroot='' &
+
+        echo "Checking that portal serves the certs"
+        FIRST_ONE_SERVED_CERT=$(openssl s_client -connect one.example:5001 </dev/null 2>/dev/null | openssl x509 -inform pem -text)
+        FIRST_TWO_SERVED_CERT=$(openssl s_client -connect two.example:5001 </dev/null 2>/dev/null | openssl x509 -inform pem -text)
+
+        killall portal
+
+        ACME_SERVER_CERT=./pebble/test/certs/pebble.minica.pem portal \
+          -autocert_domains=one.example,two.example \
+          -autocert_server=https://127.0.0.1:14000/dir \
+          -http_port=5002 \
+          -https_port=5001 \
+          -cert_challenge_webroot='' &
+
+        echo "Checking that portal loaded the same certs"
+        SECOND_ONE_SERVED_CERT=$(openssl s_client -connect one.example:5001 </dev/null 2>/dev/null | openssl x509 -inform pem -text)
+        SECOND_TWO_SERVED_CERT=$(openssl s_client -connect two.example:5001 </dev/null 2>/dev/null | openssl x509 -inform pem -text)
+        diff <(echo "$FIRST_ONE_SERVED_CERT") <(echo "$SECOND_ONE_SERVED_CERT")
+        diff <(echo "$FIRST_TWO_SERVED_CERT") <(echo "$SECOND_TWO_SERVED_CERT")
+
+        killall portal
+        killall pebble
 
   test-certbot-certs:
     runs-on: ubuntu-latest

portal/embedportal/acme.go
@@ -4,9 +4,13 @@ import (
 	"context"
 	"crypto"
 	"crypto/tls"
+	"crypto/x509"
+	"encoding/pem"
 	"errors"
 	"fmt"
 	"log"
+	"net/http"
+	"os"
 	"sync"
 
 	"ask.systems/daemon/tools"
@@ -33,10 +37,50 @@ func (c *acmeChallenges) Delete(path string) {
 	c.Map.Delete(path)
 }
 
+func loadCACert(certFile string) *x509.CertPool {
+	certBytes, err := os.ReadFile(certFile)
+	if err != nil {
+		log.Print("Error reading ACME_SERVER_CERT file: ", err)
+		return nil
+	}
+	block, _ := pem.Decode(certBytes)
+	if block == nil {
+		log.Print("Error decoding ACME_SERVER_CERT file.")
+		return nil
+	}
+	cert, err := x509.ParseCertificate(block.Bytes)
+	if err != nil {
+		log.Print("Error parsing ACME_SERVER_CERT file: ", err)
+		return nil
+	}
+	pool, err := x509.SystemCertPool()
+	if err != nil {
+		log.Print("Error reading system CA roots: ", err)
+		return nil
+	}
+	pool.AddCert(cert)
+	return pool
+}
+
 func acmeClient(accountKey crypto.Signer) *acme.Client {
+	client := http.DefaultClient
+	if serverCertFile := os.Getenv("ACME_SERVER_CERT"); serverCertFile != "" {
+		log.Print("Loading test CA root file for ACME server testing: ", serverCertFile)
+		roots := loadCACert(serverCertFile)
+		if roots != nil {
+			transport := http.DefaultTransport.(*http.Transport).Clone()
+			transport.TLSClientConfig = &tls.Config{
+				RootCAs: roots,
+			}
+			client = &http.Client{
+				Transport: transport,
+			}
+		}
+	}
 	return &acme.Client{
 		Key:          accountKey,
-		DirectoryURL: acme.LetsEncryptURL,
+		DirectoryURL: kACMEAddress,
+		HTTPClient:   client,
 		UserAgent:    "daemon portal",
 	}
 }
@@ -50,13 +94,11 @@ func fetchACMEAccount(client *acme.Client) (*acme.Account, error) {
 		if errors.Is(err, acme.ErrNoAccount) {
 			account, err = client.Register(ctx, &acme.Account{}, acme.AcceptTOS)
 			if err != nil {
-				err = fmt.Errorf("acme.Register error: %w", err)
+				return nil, fmt.Errorf("acme.Register error: %w", err)
 			}
+		} else {
+			return nil, fmt.Errorf("acme.GetReg error: %w", err)
 		}
-		return nil, fmt.Errorf("acme.GetReg error: %w", err)
-	}
-	if err != nil {
-		return nil, err
 	}
 	return account, nil
 }

portal/embedportal/portal.go
@@ -18,6 +18,7 @@ import (
 	"time"
 
 	_ "ask.systems/daemon/tools/flags"
+	"golang.org/x/crypto/acme"
 
 	"ask.systems/daemon/tools"
 )
@@ -29,6 +30,8 @@ const (
 	leaseTTL         = 24 * time.Hour
 	ttlRandomStagger = 0.05
 )
+
+var kACMEAddress string
 
 func Run(flags *flag.FlagSet, args []string) {
 
@@ -84,6 +87,12 @@ func Run(flags *flag.FlagSet, args []string) {
 		"certificates for. By default provided freely by https://letsencrypt.org.\n"+
 		"By using this feature you accept their TOS.\n\n"+
 		"If you use this you don't need to set any other tls or cert related flags.")
+	acmeAddress := flags.String("autocert_server", acme.LetsEncryptURL, ""+
+		"The ACME server directory URL to use when automatically obtaining TLS\n"+
+		"certificates. This is uncommon. There are alternate providers, private\n"+
+		"servers, and this is useful for testing. For testing only you can also\n"+
+		"set the ACME_SERVER_CERT env var to a PEM encoded CA cert file for\n"+
+		"connecting to the autocert server over HTTPS.\n")
 	tlsCertSpec := flags.String("tls_cert", "", ""+
 		"The filepath to the tls cert file (fullchain.pem).\n"+
 		"Accepts multiple certificates with a comma separated list.\n"+
@@ -116,6 +125,7 @@ func Run(flags *flag.FlagSet, args []string) {
 		"The path to the file to store active lease information in so that\n"+
 		"the portal server can safely restart without disrupting proxy service.\n")
 	flags.Parse(args[1:])
+	kACMEAddress = *acmeAddress
 
 	quit := make(chan struct{})
 	tools.CloseOnQuitSignals(quit)